e7c384e67a5a206d62ae009d2222aaed9e49232c
thambley <thambley@travelleaders.com>
2014-07-01 12:34:20 -0400

    #3905 add tests

--- test/unit/textile_converter_test.rb
+++ test/unit/textile_converter_test.rb
@@ -1,11 +1,34 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class TextileConverterTest < ActiveSupport::TestCase
-  #fixtures :html_strings
 
-  test "simple bold text conversion" do
+  # simple html to text conversion tests:
+  
+  test "bold text" do
     simple_bold_text = RedmineHtmlToWikiMailHandler::HtmlToWikiFormatting::Textile::Formatter.new("<b>test</b>").to_wiki
-    assert_equal "*test*", simple_bold_text, "Expected *test*"
+    assert_equal "*test*", 
+                 simple_bold_text,
+                 "Expected: *test*"
   end
-
+  
+  test "header level 1" do
+    header_text = RedmineHtmlToWikiMailHandler::HtmlToWikiFormatting::Textile::Formatter.new("<h1>Give RedCloth a try!</h1>").to_wiki
+    assert_equal "h1. Give RedCloth a try!",
+                 header_text,
+                 "Expected: h1. Give RedCloth a try!"
+  end
+  
+  # more complicated conversions:
+  
+  test "paragraph with some formatting" do
+    paragraph_text = RedmineHtmlToWikiMailHandler::HtmlToWikiFormatting::Textile::Formatter.new("<p>A <strong>simple</strong> paragraph with<br />\na line break, some <em>emphasis</em> and a <a href=\"http://redcloth.org\">link</a></p>").to_wiki
+    assert_equal "A *simple* paragraph with\na line break, some _emphasis_ and a \"link\":http://redcloth.org",
+                 paragraph_text
+  end
+  
+  test "lists" do
+    list_text = RedmineHtmlToWikiMailHandler::HtmlToWikiFormatting::Textile::Formatter.new("<ul>\n<li>an item</li>\n	<li>and another</li>\n</ul>\n<ol>\n	<li>one</li>\n	<li>two</li>\n</ol>").to_wiki
+    assert_equal "* an item\n* and another\n\n\n# one\n# two",
+                 list_text
+  end
 end
