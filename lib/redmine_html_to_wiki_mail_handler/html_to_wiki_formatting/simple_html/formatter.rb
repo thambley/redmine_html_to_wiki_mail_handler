15ca0245fed009a969b25e0a299a0f4ef6758867
thambley <thambley@travelleaders.com>
2014-07-11 11:32:44 -0400

    improve handling of styles and neighboring formatting nodes in textile ** -> *<notextile></notextile>*

--- lib/redmine_html_to_wiki_mail_handler/html_to_wiki_formatting/simple_html/formatter.rb
+++ lib/redmine_html_to_wiki_mail_handler/html_to_wiki_formatting/simple_html/formatter.rb
@@ -8,31 +8,55 @@ module RedmineHtmlToWikiMailHandler
   
         def initialize(html)
           super(html)
-          @workingcopy = html
+          @working_copy = html
         end
     
         def to_wiki(*rules)
           # load html fragment
-          doc = Nokogiri::HTML.fragment(@workingcopy) do |config|
+          doc = Nokogiri::HTML.fragment(@working_copy) do |config|
             config.noblanks
           end
           
           # Remove all style content
           doc.xpath('.//style').each{ |n| n.remove }
           
-          wiki_text = process_node(doc, false)
+          wiki_text = "<div>#{process_node(doc, false)}</div>"
   
           clean_up_nbsp_for_formatting(remove_extra_formatting(wiki_text)).gsub(160.chr(Encoding::UTF_8),"&nbsp;")
         end
         
         private
         
+        STYLES_RE = /^(color|width|height|border|background|padding|margin|font|text|float)(-[a-z]+)*:\s*((\d+%?|\d+px|\d+(\.\d+)?em|#[0-9a-f]+|[a-z]+)\s*)+$/i
+        
+        def sanitize_styles(str)
+          styles = str.split(";").map(&:strip)
+          styles.reject! do |style|
+            !style.match(STYLES_RE)
+          end
+          styles.join(";")
+        end
+        
+        def filtered_style_attribute(node)
+          style_string = ""
+          node_style = node[:style]
+          if node_style
+            filtered_style = sanitize_styles(node_style)
+            if filtered_style.length > 0
+              style_string = "style=\"#{filtered_style}\""
+            end 
+          end
+          style_string
+        end
+        
         def process_node(node, process_text)
           node_html = ''
           if node.element?
             case node.node_name
-            when 'p','div','h1','h2','h3','h4','h5','h6','h7','h8','h9','b','strong','i','em','u','strike','td','li','span'
+            when 'p','div','h1','h2','h3','h4','h5','h6','h7','h8','h9','b','strong','i','em','u','strike','td','li'
               node_html.concat(process_content_node(node, true))
+            when 'span'
+              node_html.concat(process_span_node(node))
             when 'table','tr','ul','ol'
               node_html.concat(process_content_node(node, false))
             when 'br','hr'
@@ -51,15 +75,31 @@ module RedmineHtmlToWikiMailHandler
           end
           node_html
         end
+        
+        def process_span_node(node)
+          # find style properties.  If no acceptable style properties are found, just output text
+          # at this time, only color and background color are acceptable
+          simple_html_text = "<span #{filtered_style_attribute(node)}>"
+          
+          if simple_html_text == "<span >"
+            simple_html_text = ''
+            node.children.each {|n| simple_html_text.concat(process_node(n, true))}            
+          else
+            node.children.each {|n| simple_html_text.concat(process_node(n, true))}
+            simple_html_text.concat("</span>")
+          end
+          
+          simple_html_text
+        end
 
         def process_content_node(node, process_text)
-          simple_html_text = "<#{node.name}>"
+          simple_html_text = "<#{node.name} #{filtered_style_attribute(node)}>"
           node.children.each {|n| simple_html_text.concat(process_node(n, process_text))}
           simple_html_text.concat("</#{node.name}>")
           simple_html_text
         end
 
-        def process_simple_node(node) 
+        def process_simple_node(node)
           "<#{node.name} />"
         end
 
--- lib/redmine_html_to_wiki_mail_handler/html_to_wiki_formatting/textile/formatter.rb
+++ lib/redmine_html_to_wiki_mail_handler/html_to_wiki_formatting/textile/formatter.rb
@@ -37,11 +37,15 @@ module RedmineHtmlToWikiMailHandler
   
         def initialize(html)
           super(html)
-          @workingcopy = html
+          @working_copy = html
+          @in_table = false
+          @list_depth = 0
+          @list_character = ''
+          #@in_pre = false
         end
     
         def to_wiki(*rules)
-          html = RedmineHtmlToWikiMailHandler::HtmlToWikiFormatting.formatter_for(:simple_html).new(@workingcopy).to_wiki
+          html = RedmineHtmlToWikiMailHandler::HtmlToWikiFormatting.formatter_for(:simple_html).new(@working_copy).to_wiki
           
           # load html fragment
           doc = Nokogiri::HTML.fragment(html) do |config|
@@ -51,9 +55,11 @@ module RedmineHtmlToWikiMailHandler
           # Remove all style content
           doc.xpath('.//style').each{ |n| n.remove }
           
-          wiki_text = process_node(doc, {:table? => false, :list_depth => 0, :list_character => '', :pre? => false, :bold => '', :italic => '', :underline => '', :strike => ''}, false).rstrip
+          wiki_text = process_node(doc, false).rstrip
   
           wiki_text.gsub!(160.chr(Encoding::UTF_8),"&nbsp;")
+          
+          wiki_text.gsub!(/([*-+%_])\1/,'\1<notextile></notextile>\1')
           
           #return 
           wiki_text.lines.map{|line| update_line(line)}.join
@@ -70,14 +76,34 @@ module RedmineHtmlToWikiMailHandler
           updated_line_text.gsub!(/^o&nbsp;&nbsp; /,"** ") # second level bullet
           updated_line_text.gsub!(/^[\u00A7]&nbsp; /,"*** ") # 3rd level bullet (section entity)
           
-          updated_line_text.gsub!(/^[0-9]+.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; /, "# ")
+          updated_line_text.gsub!(/^[0-9]+\.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; /, "# ")
           
           updated_line_text
         end
         
-        def process_paragraph_node(node, state_info)
+        def apply_formatting(text, format, style)
+          formatted_text = ''
+          
+          if style && style.length > 0
+            text_style = "{#{style}}"
+          else
+            text_style = ''
+          end
+          
+          text.each_line do |line|
+            prepend_spaces, node_words, append_spaces = extract_node_words( text )
+            if node_words.length > 0
+              formatted_text << prepend_spaces << format << text_style << node_words << format << append_spaces
+            else
+              formatted_text << prepend_spaces
+            end
+          end
+          formatted_text
+        end
+        
+        def process_paragraph_node(node)
           node_text = ''
-          node.children.each {|n| node_text.concat(process_node(n, state_info, true))}
+          node.children.each {|n| node_text.concat(process_node(n, true))}
           node_text.gsub!(/[ \u00A0]+$/,'')
           node_text.gsub!(/^[ ]+/,'')
           node_text.concat("\n")
@@ -85,84 +111,76 @@ module RedmineHtmlToWikiMailHandler
           node_text
         end
         
-        def process_span_node(node, state_info)
+        def process_span_node(node)
           node_text = ''
-          node.children.each {|n| node_text.concat(process_node(n, state_info, true))}
-          node_text
+          node.children.each {|n| node_text.concat(process_node(n, true))}
+          apply_formatting(node_text, '%', node[:style])
         end
 
-        def process_header_node(node, state_info)
+        def process_header_node(node)
           node_text = ''
           node_text.concat("#{node.node_name}. ")
-          node.children.each {|n| node_text.concat(process_node(n, state_info, true))}
+          node.children.each {|n| node_text.concat(process_node(n, true))}
           node_text.concat("\n\n")
           node_text
         end
 
-        def process_bold_node(node, state_info)
+        def process_bold_node(node)
           node_text = ''
-          state_info[:bold] = '*'
-          node.children.each {|n| node_text.concat(process_node(n, state_info, true))}
-          state_info[:bold] = ''
-          
-          node_text
+          node.children.each {|n| node_text.concat(process_node(n, true))}
+          apply_formatting(node_text, '*', node[:style])
         end
 
-        def process_italic_node(node, state_info)
+        def process_italic_node(node)
           node_text = ''
-          state_info[:italic] = '_'
-          node.children.each {|n| node_text.concat(process_node(n, state_info, true))}
-          state_info[:italic] = ''
-          node_text
+          node.children.each {|n| node_text.concat(process_node(n, true))}
+          apply_formatting(node_text, '_', node[:style])
         end
 
-        def process_underline_node(node, state_info)
+        def process_underline_node(node)
           node_text = ''
-          state_info[:underline] = '+'
-          node.children.each {|n| node_text.concat(process_node(n, state_info, true))}
-          state_info[:underline] = ''
-          node_text
+          node.children.each {|n| node_text.concat(process_node(n, true))}
+          apply_formatting(node_text, '+', node[:style])
         end
 
-        def process_strike_node(node, state_info)
+        def process_strike_node(node)
           node_text = ''
-          state_info[:strike] = '-'
-          node.children.each {|n| node_text.concat(process_node(n, state_info, true))}
-          state_info[:strike] = ''
-          node_text
+          node.children.each {|n| node_text.concat(process_node(n, true))}
+          apply_formatting(node_text, '-', node[:style])
         end
 
-        def process_table_node(node, state_info)
+        def process_table_node(node)
           node_text = ''
-          state_info[:table?] = true
-          node.children.each {|n| node_text.concat(process_node(n, state_info, false))}
-          state_info[:table?] = false
+          was_in_table = @in_table
+          @in_table = true
+          node.children.each {|n| node_text.concat(process_node(n, false))}
+          @in_table = was_in_table
           node_text.concat("\n")
           node_text
         end
 
-        def process_table_row_node(node, state_info)
+        def process_table_row_node(node)
           node_text = ''
-          node.children.each {|n| node_text.concat(process_node(n, state_info, false))}
+          node.children.each {|n| node_text.concat(process_node(n, false))}
           node_text.concat("|\n")
           node_text
         end
 
-        def process_table_data_node(node, state_info)
+        def process_table_data_node(node)
           node_text = ''
           node_text.concat("|")
-          node.children.each {|n| node_text.concat(process_node(n, state_info, true))}
+          node.children.each {|n| node_text.concat(process_node(n, true))}
           node_text.gsub!(/[\s]+$/,'')
           node_text
         end
 
-        def process_line_break_node(node, state_info)
+        def process_line_break_node(node)
           node_text = ''
           node_text.concat("\n")
           node_text
         end
 
-        def process_hrule_node(node, state_info)
+        def process_hrule_node(node)
           node_text = ''
           node_text.concat("\n---\n\n")
           node_text
@@ -186,13 +204,13 @@ module RedmineHtmlToWikiMailHandler
           alt_text
         end
 
-        def process_image_node(node, state_info)
+        def process_image_node(node)
           "!#{image_filename(node)}#{image_alt(node)}!"
         end
 
-        def process_link_node(node, state_info)
+        def process_link_node(node)
           link_text = ''
-          node.children.each {|n| link_text.concat(process_node(n, state_info, true))}
+          node.children.each {|n| link_text.concat(process_node(n, true))}
           
           if m = /^(?<prepend_spaces>[\s\u00A0]*)(?<image_text>[\!][^\!]+[\!])(?<append_spaces>[\s\u00A0]*)$/.match(link_text)
             link_text = "#{m[:prepend_spaces]}#{m[:image_text]}:#{node[:href]}#{m[:append_spaces]}"
@@ -210,28 +228,28 @@ module RedmineHtmlToWikiMailHandler
           link_text
         end
 
-        def process_list_node(node, state_info, list_character)
+        def process_list_node(node, list_character)
           node_text = ''
           
-          previous_list_character = state_info[:list_character]
-          state_info[:list_character] = list_character
-          state_info[:list_depth] += 1
+          previous_list_character = @list_character
+          @list_character = list_character
+          @list_depth += 1
           
-          node.children.each {|n| node_text.concat(process_node(n, state_info, true))}
+          node.children.each {|n| node_text.concat(process_node(n, true))}
           
-          state_info[:list_depth] -= 1
-          state_info[:list_character] = previous_list_character
+          @list_depth -= 1
+          @list_character = previous_list_character
           
           node_text.concat("\n")
           node_text
         end
 
-        def process_list_item_node(node, state_info)
+        def process_list_item_node(node)
           node_text = ''
-          node.children.each {|n| node_text.concat(process_node(n, state_info, true))}
+          node.children.each {|n| node_text.concat(process_node(n, true))}
          
           if !/^[\n]*([\#\*])\1* /.match(node_text)
-            node_text = "#{state_info[:list_depth].times.collect {state_info[:list_character]}.join('')} #{node_text}\n"
+            node_text = "#{@list_depth.times.collect {@list_character}.join('')} #{node_text}\n"
           end
           
           node_text
@@ -262,10 +280,6 @@ module RedmineHtmlToWikiMailHandler
           replace_special_characters(replace_characters(replace_entities(node_text)))
         end
 
-        def font_modifiers(state_info)
-          "#{state_info[:underline]}#{state_info[:strike]}#{state_info[:bold]}#{state_info[:italic]}"
-        end
-
         def extract_node_words(node_words)
           space_matches = /^(?<spaces>[\s\u00A0]+)/.match(node_words)
           
@@ -288,69 +302,57 @@ module RedmineHtmlToWikiMailHandler
           [prepend_spaces, node_words, append_spaces]
         end
 
-        def process_text_node(node, state_info)
-          node_text = ''
-          
-          prepend_spaces, node_words, append_spaces = extract_node_words( node.to_s.gsub(/[\r\n]/,' ').squeeze(" ") )
-          
-          start_font_modifier = font_modifiers(state_info)
-          
-          if node_words.length > 0
-            node_text << prepend_spaces << start_font_modifier << clean_up_node_words(node_words) << start_font_modifier.reverse << append_spaces
-          else
-            node_text << prepend_spaces
-          end
-          
-          node_text
+        def process_text_node(node)
+          clean_up_node_words(node.to_s.gsub(/[\r\n]/,' ').squeeze(" "))
         end
 
         # recurive html node processor
         # returns wiki text
-        def process_node(node, state_info, process_text)
+        def process_node(node, process_text)
           node_text = ''
           if node.element?
             case node.node_name
             when 'p', 'div'
-              node_text.concat(process_paragraph_node(node, state_info))
+              node_text.concat(process_paragraph_node(node))
             when 'span'
-              node_text.concat(process_span_node(node, state_info))
+              node_text.concat(process_span_node(node))
             when 'h1','h2','h3','h4','h5','h6','h7','h8','h9'
-              node_text.concat(process_header_node(node, state_info))
+              node_text.concat(process_header_node(node))
             when 'b', 'strong'
-              node_text.concat(process_bold_node(node, state_info))
+              node_text.concat(process_bold_node(node))
             when 'i', 'em'
-              node_text.concat(process_italic_node(node, state_info))
+              node_text.concat(process_italic_node(node))
             when 'u'
-              node_text.concat(process_underline_node(node, state_info))
+              node_text.concat(process_underline_node(node))
             when 'strike'
-              node_text.concat(process_strike_node(node, state_info))
+              node_text.concat(process_strike_node(node))
             when 'br'
-              node_text.concat(process_line_break_node(node, state_info))
+              node_text.concat(process_line_break_node(node))
             when 'hr'
-              node_text.concat(process_hrule_node(node, state_info))
+              node_text.concat(process_hrule_node(node))
             when 'table'
-              node_text.concat(process_table_node(node, state_info))
+              node_text.concat(process_table_node(node))
             when 'tr'
-              node_text.concat(process_table_row_node(node, state_info))
+              node_text.concat(process_table_row_node(node))
             when 'td'
-              node_text.concat(process_table_data_node(node, state_info))
+              node_text.concat(process_table_data_node(node))
             when 'ul'
-              node_text.concat(process_list_node(node, state_info, '*'))
+              node_text.concat(process_list_node(node, '*'))
             when 'ol'
-              node_text.concat(process_list_node(node, state_info, '#'))
+              node_text.concat(process_list_node(node, '#'))
             when 'li'
-              node_text.concat(process_list_item_node(node, state_info))
+              node_text.concat(process_list_item_node(node))
             when 'img'
-              node_text.concat(process_image_node(node, state_info))
+              node_text.concat(process_image_node(node))
             when 'a'
-              node_text.concat(process_link_node(node, state_info))
+              node_text.concat(process_link_node(node))
             else
-              node.children.each {|n| node_text.concat(process_node(n, state_info, process_text))}
+              node.children.each {|n| node_text.concat(process_node(n, process_text))}
             end
           elsif process_text and node.text?
-            node_text.concat(process_text_node(node, state_info))
+            node_text.concat(process_text_node(node))
           else
-            node.children.each {|n| node_text.concat(process_node(n, state_info, process_text))}
+            node.children.each {|n| node_text.concat(process_node(n, process_text))}
           end
           node_text
         end
