646bd65aaedaceaf0b2b7e38e84db021b311ad47
thambley <thambley@travelleaders.com>
2014-07-13 02:00:53 -0400

    refactor for code coverage

--- lib/redmine_html_to_wiki_mail_handler.rb
+++ lib/redmine_html_to_wiki_mail_handler.rb
@@ -2,19 +2,9 @@ require 'redmine_html_to_wiki_mail_handler/html_to_wiki_formatting'
 require 'redmine_html_to_wiki_mail_handler/html_to_wiki_formatting/simple_html/formatter'
 require 'redmine_html_to_wiki_mail_handler/html_to_wiki_formatting/textile/formatter'
 #require 'redmine_html_to_wiki_mail_handler/html_to_wiki_formatting/markdown/formatter'
-
-require 'dispatcher' unless Rails::VERSION::MAJOR >= 3
  
-if Rails::VERSION::MAJOR >= 3
-   ActionDispatch::Callbacks.to_prepare do
-     # use require_dependency if you plan to utilize development mode
-     require 'redmine_html_to_wiki_mail_handler/patches/mail_handler_patch'
-   end
-else
-  Dispatcher.to_prepare do
-    # use require_dependency if you plan to utilize development mode
-    require 'redmine_html_to_wiki_mail_handler/patches/mail_handler_patch'
-  end
+RedmineApp::Application.config.after_initialize do
+  require_dependency 'redmine_html_to_wiki_mail_handler/patches/mail_handler_patch'
 end
 
 
