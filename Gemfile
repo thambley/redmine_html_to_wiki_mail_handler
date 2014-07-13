298dfbe93b7ba091582b8e6dc9f0eb2b0afb23d9
thambley <thambley@travelleaders.com>
2014-07-13 00:28:57 -0400

    add coveralls

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'nokogiri', '>= 1.5.10'
+
+gem 'coveralls', require: false
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,5 @@
+require 'coveralls'
+Coveralls.wear!
 # Load the normal Rails helper
 require File.expand_path('../../../../test/test_helper', __FILE__)
 #require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
