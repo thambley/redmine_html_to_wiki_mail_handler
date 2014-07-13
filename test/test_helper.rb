42ed9d0ab523283e3230fc611a5549e6bada1088
thambley <thambley@travelleaders.com>
2014-07-13 01:00:40 -0400

    changed coveralls code

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,15 @@
-require 'coveralls'
-Coveralls.wear!
+require 'simplecov'
+
+SimpleCov.start do
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Models', 'app/models'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Libraries', 'lib'
+  add_filter '/test/'
+  add_filter 'init.rb'
+  root File.expand_path(File.dirname(__FILE__) + '/../')
+end if RUBY_VERSION >= '1.9.0'
+
 # Load the normal Rails helper
 require File.expand_path('../../../../test/test_helper', __FILE__)
 #require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
